e9fe11c61840c7f0d9b4e4137cdb696afaca12da
DavidHerreros <david.herreroscalero@gmail.com>
2022-05-27 16:31:48 +0200

    Add header to file

--- flexutils/protocols/xmipp/scripts/find_z_clnm_structure.py
+++ flexutils/protocols/xmipp/scripts/find_z_clnm_structure.py
@@ -1,7 +1,35 @@
+# **************************************************************************
+# *
+# * Authors:  David Herreros Calero (dherreros@cnb.csic.es)
+# *
+# * Unidad de  Bioinformatica of Centro Nacional de Biotecnologia , CSIC
+# *
+# * This program is free software; you can redistribute it and/or modify
+# * it under the terms of the GNU General Public License as published by
+# * the Free Software Foundation; either version 2 of the License, or
+# * (at your option) any later version.
+# *
+# * This program is distributed in the hope that it will be useful,
+# * but WITHOUT ANY WARRANTY; without even the implied warranty of
+# * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# * GNU General Public License for more details.
+# *
+# * You should have received a copy of the GNU General Public License
+# * along with this program; if not, write to the Free Software
+# * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+# * 02111-1307  USA
+# *
+# *  All comments concerning this program package may be sent to the
+# *  e-mail address 'scipion@cnb.csic.es'
+# *
+# **************************************************************************
+
+
 import os
 import numpy as np
 import prody as pd
 import flexutils.protocols.xmipp.utils.utils as utl
+
 
 def computeZernikeCoefficientsStructure(file_input, file_target, file_output, L1, L2):
     start = pd.parsePDB(file_input, subset='ca', compressed=False)
