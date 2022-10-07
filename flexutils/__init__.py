5296bea3365304ed15b1445928577ec525fc62d3
DavidHerreros <david.herreroscalero@gmail.com>
2022-10-07 16:09:26 +0200

    Fix prody plugin name

--- flexutils/__init__.py
+++ flexutils/__init__.py
@@ -73,6 +73,8 @@ class Plugin(pwplugin.Plugin):
                     package_name = package.replace("scipion-em-", "")
                     if "xmipp" in package_name:
                         package_name += "3"
+                    if "prody" in package_name:
+                        package_name += "2"
                     package = importlib.import_module(package_name)
                     package_env_vars = package.Plugin.getVars()
                     for item, value in package_env_vars.items():
