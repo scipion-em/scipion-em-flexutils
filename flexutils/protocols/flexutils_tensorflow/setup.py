34d1471583ceee749aa0e6c789b05814898f979d
DavidHerreros <david.herreroscalero@gmail.com>
2022-11-21 13:17:32 +0100

    init files and setup

---
+++ flexutils/protocols/flexutils_tensorflow/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(name='flexutils_tensorflow',
+      version="1.0.0",  # Required
+      description='Xmipp tensorflow utilities for flexibility',
+      author='David Herreros',
+      author_email='dherreros@cnb.csic.es',
+      keywords='scipion continuous-heterogeneity imageprocessing xmipp',
+      packages=find_packages())
