23749b882873c03f29e7b9fbf67a341da746a49b
DavidHerreros <david.herreroscalero@gmail.com>
2023-03-15 10:35:02 +0100

    New implementation of Flexutils objects

--- flexutils/protocols/__init__.py
+++ flexutils/protocols/__init__.py
@@ -24,6 +24,7 @@
 # *  e-mail address 'scipion@cnb.csic.es'
 # *
 # **************************************************************************
+from .protocol_base import ProtFlexBase
 
 from .protocol_select_views import ProtFlexSelectViews
 from .protocol_annotate_space import ProtFlexAnnotateSpace
