d0559ad5874f42fa364fe9cac976ab52264b98d8
DavidHerreros <david.herreroscalero@gmail.com>
2024-02-07 14:17:16 +0100

    Find best conformational ordering for a set of volumes based on its Z and TSP

--- flexutils/protocols/__init__.py
+++ flexutils/protocols/__init__.py
@@ -32,6 +32,7 @@ from .protocol_dimred import ProtFlexDimRedSpace
 from .protocol_find_optimal_clusters import ProtFlexOptimalClusters
 from .protocol_associate_flex_space_to_particles import ProtFlexAssociateSpace
 from .protocol_score_landscape import ProtFlexScoreLandscape
+from .protocol_order_volumes import ProtFlexOrderVolumes
 
 # Zernike3D Xmipp protocols (offered here due to strong package dependency)
 from .xmipp import *
